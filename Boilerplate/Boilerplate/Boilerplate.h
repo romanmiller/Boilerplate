f6a13e72d3972a9c6f54b5187a939a102f34cba8
Roman <romanstl1208@gmail.com>
2019-05-02 16:41:26 +0300

    first init of Boilerplate framework

---
+++ Boilerplate/Boilerplate/Boilerplate.h
@@ -0,0 +1,19 @@
+//
+//  Boilerplate.h
+//  Boilerplate
+//
+//  Created by Mac on 5/2/19.
+//  Copyright © 2019 RS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boilerplate.
+FOUNDATION_EXPORT double BoilerplateVersionNumber;
+
+//! Project version string for Boilerplate.
+FOUNDATION_EXPORT const unsigned char BoilerplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boilerplate/PublicHeader.h>
+
+
